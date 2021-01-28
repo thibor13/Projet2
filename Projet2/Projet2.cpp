451689e1ef4fe40230f48b3d0f7dba8a474b8246
thibault <warrior.callofdutty@gmail.com>
2021-01-28 20:03:59 +0100

    spawn this project

---
+++ Projet2/Projet2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+
